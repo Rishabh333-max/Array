32996c41fc1425db8ff268069cc7190efcd3539c
Rishabh333-max <81370987+Rishabh333-max@users.noreply.github.com>
2021-06-14 15:10:32 +0530

    array created

---
+++ js/rishabh.js
@@ -0,0 +1,118 @@
+//Array and Object
+
+// #arrays
+// sometimes we need collection of variable instead of variable so that's why we need array
+
+// const marks=[34,23,234,73,89];
+// const fruits=["orange","apple"];
+// const mixed=['str',89,[3,5]];
+// const Rishabh = new Array(23,123,21,"orange");
+// Rishabh.push(35);
+// console.log(Rishabh);
+// console.log(mixed);
+// console.log(fruits);
+
+
+// const marks=[34,23,234,73,89];
+// const fruits=["orange","apple"];
+// console.log(fruits[0]);
+
+
+// const marks=[28,29,60,84,33,34];
+// console.log(marks[3]);
+
+
+//Properties of Array
+
+//  # .length
+// const fruits=[2,4,5,6,8,9];
+// console.log(fruits.length);
+
+//  # .push
+// const marks=[34,2,3,4];
+// marks.push(234);
+// console.log(marks);
+
+//  # .isArray
+// const Rishabh = new Array(23,123,21,"orange");
+// console.log(Array.isArray(Rishabh));
+
+// const arr=new Array(23,43,54,65);
+// console.log(Array.isArray(arr));
+
+//   # .indexOf
+// const fruits=[2,4,5,6,8,9];
+// let value=fruits.indexOf(4);
+// console.log(value)
+
+//  # .Unshift
+// const fruits=[2,4,5,6,8,9];
+// fruits.unshift(100);
+// console.log(fruits);
+// console.log(fruits.unshift(100));
+
+//  # .pop()
+// const fruits=[2,4,5,6,8,9];
+// fruits.pop();
+// console.log(fruits)
+// console.log(fruits.pop());
+
+//  #shift()
+// const fruits=[2,4,5,6,8,9];
+// fruits.shift()
+// console.log(fruits);
+// console.log(fruits.shift());
+
+//  # .splice
+// const fruits=[2,4,5,6,8,9];
+// fruits.splice(1,3);
+// console.log(fruits);
+// console.log(fruits.splice(1,3))
+
+// const fruits=[2,4,5,6,8,9];
+// fruits.splice(1,3,5);
+// console.log(fruits);
+// console.log(fruits.splice(1,3))
+
+// const fruits=[2,4,5,6,8,9];
+// fruits.splice(1,3,5,"Ambesh",100);
+// console.log(fruits);
+// console.log(fruits.splice(1,3))
+
+//  # .reverse
+// const fruits=[2,4,5,6,8,9];
+// fruits.reverse();
+// console.log(fruits);
+// console.log(fruits.reverse())
+
+
+//  #OBJECT----->
+// let myProject={
+//     name:'TodoList',
+//     'owner Name':'RIshabh',
+//     Users:'Public',
+// limit:50
+// }
+// console.log(myProject);
+// console.log(myProject['owner Name']);
+// console.log(myProject.limit)
+
+
+//concat----->
+// let marks=[34,23,234,73,89];
+// let marks2=[1,2,3,7];
+// marks=marks.concat(marks2);
+// console.log(marks);
+// console.log(marks.concat(marks2))
+
+
+//object----->
+// let myobj={
+//     name:'Rishabh',
+//     degree:'Btech',
+//     gfName:'Khushi',
+//     marks:[12,13,20,19]
+// }
+// console.log(myobj);
+// console.log(myobj.name)
+
